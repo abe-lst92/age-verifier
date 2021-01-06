afed3a6b46656719f1f419f1ad9d885eff580ef5
abe-lst92 <abraham.sauz7@gmail.com>
2021-01-05 18:43:26 -0700

    Oh noooooo

--- javas.js
+++ javas.js
@@ -1,39 +1,123 @@
-alert("Are you sure you want to go to the next page?");
+var ferrari={
+    type: "Ferrari",
+    model: "Sf90",
+    available: "Yes there are many of this",
+    description: "A good ferrari to enjoy the speed and it looks very luxury when you look at it, It specific details it looks decent"
 
-   var num =  prompt("Okay, First tell me your age?" );
-
-
-function ageTeller(){
-num
-    if(num >= 18){
-    alert("Acces confirm!");
-} else if(num <= 17){
-    alert("Acces denied!");
-
-};
-};
-
-ageTeller();
-
-
-
-var str = "Squirt";
-
-switch (str) 
-{
-    case "Squirt":
-        alert("This is my favorite coke");
-    case "coca-cola":
-        alert("This is my second favorite coke");
-        break;
-    case "mtdw":
-        alert("This is the third soda that i like");
-        break;
-    default:
-        alert("Unknown coke");
-        break;
 }
-  document.getElementById("demo").innerHTML = str;
+
+
+
+function Info(){
+    var design = document.getElementById("butt");
+
+
+    for(value in ferrari){
+        design.innerHTML = "Type " + ferrari.type + "</br> " +
+        "Model " + ferrari.model + " </br> " + "Available " + ferrari.available + "</br>" + " Description " + ferrari.description;
+    }
+
+
+  
+
+   
+}
+var lamborghini={
+    type: "Lamborghini",
+    model: "Aventador J",
+    available: "In lamborguinis's store",
+    description: "I am a good car to enjoy and get distracted for a while"
+
+}
+function info2(){
+    var design2 = document.getElementById("details2");
+
+    for(value in lamborghini){
+        design2.innerHTML = "Type " + ferrari.type + "<br>" +
+        "Model " + ferrari.model + " </br> " + "Available " + ferrari.available + "</br>" + " Description " + ferrari.description;
+    }
+}
+
+var whippet={
+    type: "Classic truck",
+    model: "1927 Willys-Overland Whippet 93A",
+    available: "Not that many",
+    description: "I'm not fast but I seem so funny."
+
+}
+ function info3(){
+    var design3 = document.getElementById("details3");
+
+    for(value in whippet){
+        design3.innerHTML = "Type " + ferrari.type + "</br>" +
+        "Model " + ferrari.model + "</br>" + "Available " + ferrari.available + "</br>" + " Description " + ferrari.description;
+    }
+
+ }
+
+ function remover(){
+    var myobj = document.getElementById("butt");
+    myobj.remove();
+
+ }
+ function remover2(){
+    var myobj2 = document.getElementById("details2");
+    myobj2.remove();
+
+ }
+ function remover3(){
+    var myobj3 = document.getElementById("details3");
+
+     myobj3.remove();
+
+ }
+
+
+
+
+
+// alert("Are you sure you want to go to the next page?");
+
+//    var num =  prompt("Okay, First tell me your age?" );
+
+
+// function ageTeller(){
+// num
+//     if(num >= 18){
+//     alert("Acces confirm!");
+// } else if(num <= 17){
+//     alert("Acces denied!");
+
+// };
+// };
+
+// ageTeller();
+
+
+
+// var str = "Squirt";
+
+// switch (str) 
+// {
+//     case "Squirt":
+//         alert("This is my favorite coke");
+//     case "coca-cola":
+//         alert("This is my second favorite coke");
+//         break;
+//     case "mtdw":
+//         alert("This is the third soda that i like");
+//         break;
+//     default:
+//         alert("Unknown coke");
+//         break;
+// }
+//   document.getElementById("demo").innerHTML = str;
+
+
+
+
+
+
 
 // for(let i = 0; i < 10; i++){
 //     document.write("Hello World" + '<br/>');
